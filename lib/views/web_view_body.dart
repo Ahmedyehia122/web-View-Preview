a42d2afac300cfec8e88f502aac9aeffdf889d3a
Ahmed Yahia <ahmedyehia.122a@gmail.com>
2024-07-03 19:51:24 +0300

    edit controller

--- lib/views/web_view_body.dart
+++ lib/views/web_view_body.dart
@@ -1,38 +1,54 @@
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
-class WebViewBody extends StatelessWidget {
+class WebViewBody extends StatefulWidget {
   const WebViewBody({super.key});
+
+  @override
+  State<WebViewBody> createState() => _WebViewBodyState();
+}
+
+class _WebViewBodyState extends State<WebViewBody> {
+  late final WebViewController controller;
+  final String url = 'https://github.com/Ahmedyehia122';
+
+  @override
+  void initState() {
+    super.initState();
+    controller = WebViewController()
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..setBackgroundColor(const Color(0x00000000))
+      ..setNavigationDelegate(
+        NavigationDelegate(
+          onProgress: (int progress) {
+            // Update loading bar.
+          },
+          onPageStarted: (String url) {
+            print('page started : $url');
+          },
+          onPageFinished: (String url) {
+            print('page finished : $url');
+          },
+          onHttpError: (HttpResponseError error) {},
+          onWebResourceError: (WebResourceError error) {},
+          onNavigationRequest: (NavigationRequest request) {
+            if (request.url.startsWith('https://www.youtube.com/')) {
+              return NavigationDecision.prevent;
+            }
+            return NavigationDecision.navigate;
+          },
+        ),
+      )
+      ..loadRequest(
+        Uri.parse(url),
+      );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: WebViewWidget(
-          controller: WebViewController()
-            ..setJavaScriptMode(JavaScriptMode.unrestricted)
-            ..setBackgroundColor(const Color(0x00000000))
-            ..setNavigationDelegate(
-              NavigationDelegate(
-                onProgress: (int progress) {
-                  // Update loading bar.
-                },
-                onPageStarted: (String url) {},
-                onPageFinished: (String url) {},
-                onHttpError: (HttpResponseError error) {},
-                onWebResourceError: (WebResourceError error) {},
-                onNavigationRequest: (NavigationRequest request) {
-                  if (request.url.startsWith('https://www.youtube.com/')) {
-                    return NavigationDecision.prevent;
-                  }
-                  return NavigationDecision.navigate;
-                },
-              ),
-            )
-            ..loadRequest(
-              Uri.parse('https://flutter.dev'),
-            ),
-        ),
+        child: WebViewWidget(controller: controller),
       ),
     );
   }
